d49a6e0db37085f09c4ec6b29a4db0d4fdeb7a5c
Ravenwater <theo@stillwater-sc.com>
2017-03-12 17:09:51 -0400

    first attempt to create a clean interaction protocol. Not successful: ambiguity between RM and KPUs

--- examples/transfer_function.go
+++ examples/transfer_function.go
@@ -6,9 +6,21 @@ import (
 	"fmt"
 	"log"
 	"github.com/stillwater-sc/gokl"
+	"github.com/stillwater-sc/gokl/dfablas"
 )
 
 func main() {
+	// set up a CPU-side matmul operators
+	A, B, C := CPUSideMatmul()
+
+	// pretty print the data structures for visual inspection
+	log.Printf("%s\n", PrettyPrintMatrix("A", A))
+	log.Printf("%s\n", PrettyPrintMatrix("B", B))
+	log.Printf("%s\n", PrettyPrintMatrix("C", C))
+
+
+	// Now do the same operator on a remote KPU
+
 	// Initialize creates a new resource manager
 	// Does the RM work on one, or more accelerators? Multiple: the RM is the big aggregator
 	// That implies that you need to get a specific KPU from the RM
@@ -27,20 +39,43 @@ func main() {
 		gokl.Shutdown()
 	}()
 
-	A := mat64.NewDense(5,5, CreateRandom(25))
-	B := mat64.NewDense(5,5, CreateRandom(25))
+	// marshal the A and B matrices into the KPU fabric
+	var targets []*gokl.KPU = make([]*gokl.KPU, 1)
+	targets[0] = kpu
+	Akpu, err := rm.Marshal(A, targets)		// marshal a CPU data structure to a set of target KPUs
+	if err != nil {
+		log.Fatal(err)
+	}
+	Bkpu, err := rm.Marshal(B, targets)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Create the zero type input C matrix
+	var Cdescriptor gokl.DataStructureDescriptor = gokl.DataStructureDescriptor{Form:gokl.KL_DENSE_MATRIX, Elements:gokl.KL_FLOAT64, Bits:64, N:5, M:5, K:0}
+	Ckpu, err := rm.New(Cdescriptor)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Create the output D matrix to receive the results from the domain flow computation
+	var Ddescriptor gokl.DataStructureDescriptor = gokl.DataStructureDescriptor{Form:gokl.KL_DENSE_MATRIX, Elements:gokl.KL_FLOAT64, Bits:64, N:5, M:5, K:0}
+	Dkpu, err := rm.New(Ddescriptor)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// now all the input and outputs have been defined, allocated, and initialized, call the operator chain
+	dfablas.GEMM(Akpu,Bkpu, Ckpu, Dkpu)
+	// marshal the result data set back to the CPU address space
+}
 
+func CPUSideMatmul() (A,B,Cout *mat64.Dense) {
+	// create and initialize the input matrices
+	A = mat64.NewDense(5,5, CreateRandom(25))
+	B = mat64.NewDense(5,5, CreateRandom(25))
+	// define a zero size output matrix to receive the result
 	var C mat64.Dense
 	C.Mul(A,B)
-
-	log.Printf("%s\n", PrettyPrintMatrix("A", A))
-	log.Printf("%s\n", PrettyPrintMatrix("B", B))
-	log.Printf("%s\n", PrettyPrintMatrix("C", &C))
-
-	// marshal the A and B matrices into the KPU fabric
-	var targets []*gokl.KPU = make([]*gokl.KPU, 4)
-	targets[0] = kpu
-	rm.Marshal(A, targets)
+	Cout = &C
+	return
 }
 
 func CreateRandom(size int) []float64 {
