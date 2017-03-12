98ab8364e5681d7eca3a71d31ccaaa3d6850f216
Ravenwater <theo@stillwater-sc.com>
2017-03-12 16:27:58 -0400

    Introducing KPU targets and seeing where it leads

--- examples/transfer_function.go
+++ examples/transfer_function.go
@@ -12,6 +12,15 @@ func main() {
 	// Initialize creates a new resource manager
 	// Does the RM work on one, or more accelerators? Multiple: the RM is the big aggregator
 	// That implies that you need to get a specific KPU from the RM
+	// The RM then needs to gather the KPU it manages and present that information
+	// to the application.
+	// TODO: how do multiple applications run concurrently?
+	// If you have application space RMs then at the time of initialization the application
+	// gets a snapshot of the resource state. The RM should provide the theoretical resource limits
+	// and the semantcs of resource allocation/deallocation will be done in the context of the state
+	// available at the time the application calls for the allocation. This would make it possible
+	// for two or more applications to be active at the same time. If one application asks for
+	// too much resource, it would receive an error, and it will need to adapt accordingly.
 	rm := gokl.Initialize()
 	kpu := rm.GetRemote("KPU-0")
 	defer func() {
@@ -29,7 +38,9 @@ func main() {
 	log.Printf("%s\n", PrettyPrintMatrix("C", &C))
 
 	// marshal the A and B matrices into the KPU fabric
-	rm.Marshal(A)
+	var targets []*gokl.KPU = make([]*gokl.KPU, 4)
+	targets[0] = kpu
+	rm.Marshal(A, targets)
 }
 
 func CreateRandom(size int) []float64 {
